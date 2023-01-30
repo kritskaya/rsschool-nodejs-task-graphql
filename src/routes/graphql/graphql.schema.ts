5c728f11bf648392a3f922229e0067175b34b9b5
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2023-01-30 20:13:14 +0300

    refactor: refactor query graphql type

--- src/routes/graphql/graphql.schema.ts
+++ src/routes/graphql/graphql.schema.ts
@@ -1,70 +1,8 @@
 import {
-  GraphQLID,
-  GraphQLList,
-  GraphQLObjectType,
   GraphQLSchema,
 } from 'graphql';
 import { mutationType } from './types/mutationTypes';
-import {
-  allEntitiesByIdType,
-  allEntitiesType,
-  usersSubscriptionsType,
-  usersWithUsersSubscribeToAndProfileType,
-  userWithDataType,
-  userWithSubscribersAndPostsType,
-} from './types/queryTypes';
-
-const queryType = new GraphQLObjectType({
-  name: 'query',
-  fields: () => ({
-    allEntities: {
-      type: allEntitiesType,
-      resolve: () => '',
-    },
-    allEntitiesById: {
-      type: allEntitiesByIdType,
-      resolve: () => '',
-    },
-    usersWithData: {
-      type: new GraphQLList(userWithDataType),
-      resolve: (_obj, _args, context) => context.db.users.findMany(),
-    },
-    userWithDataById: {
-      type: userWithDataType,
-      args: {
-        userId: { type: GraphQLID },
-      },
-      resolve: async (_obj, { userId }, context) => {
-        const user = await context.db.users.findOne({ key: 'id', equals: userId });
-        if (!user) {
-          throw new Error('user with specified id is not found');
-        }
-        return user;
-      },
-    },
-    usersWithUsersSubscribeToAndProfile: {
-      type: new GraphQLList(usersWithUsersSubscribeToAndProfileType),
-      resolve: (_obj, _args, context) => context.db.users.findMany(),
-    },
-    userWithSubscribersAndPosts: {
-      type: userWithSubscribersAndPostsType,
-      args: {
-        userId: { type: GraphQLID },
-      },
-      resolve: async (_obj, { userId }, context) => {
-        const user = await context.db.users.findOne({ key: 'id', equals: userId })
-        if (!user) {
-          throw new Error('user with specified id is not found');
-        }
-        return user;
-      }
-    },
-    usersSubscribtions: {
-      type: new GraphQLList(usersSubscriptionsType),
-      resolve: (_obj, _args, context) => context.db.users.findMany(),
-    },
-  }),
-});
+import { queryType } from './types/queryType';
 
 const schema = new GraphQLSchema({ query: queryType, mutation: mutationType });
 
