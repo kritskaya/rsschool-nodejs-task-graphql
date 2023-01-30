b6697cf07b464b866850c41ca5f48b2359881b7a
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2023-01-30 10:17:19 +0300

    feat: add depth limit rule for graphql queries

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,9 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
 import { graphqlBodySchema } from './schema';
 import { schema } from './graphql.schema';
 import { createLoaders } from './dataloader/dataloader';
+import depthLimit = require('graphql-depth-limit');
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
@@ -21,6 +22,20 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         loaders: createLoaders(fastify.db),
       };
       const variableValues = request.body.variables;
+
+      const QUERY_DEPTH_LIMIT = 6;
+      const errors = validate(schema, parse(source), [
+        depthLimit(QUERY_DEPTH_LIMIT),
+      ]);
+
+      if (errors.length > 0) {
+        return {
+          errors: {
+            message: `query exceeds maximum operation depth of ${QUERY_DEPTH_LIMIT}`,
+          },
+          data: null,
+        };
+      }
 
       const result = await graphql({
         schema,
